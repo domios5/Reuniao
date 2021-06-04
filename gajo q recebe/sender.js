e608e9458ecd0c9a87edaa854f454592dbe015a8
domios5 <85107663+domios5@users.noreply.github.com>
2021-06-04 19:10:43 +0100

    coise

---
+++ gajo q recebe/sender.js
@@ -0,0 +1,15 @@
+const webSocket = new WebSocket("ws://Localhost:5500")
+
+webSocket.onmessage = (event) => {
+    handleSignallingData(JSON.parse(event.data))
+}
+
+function handleSignallingData(data) {
+    switch (data.type) {
+        case "answer":
+            peerConn.setRemoteDescription(data.answer)
+            break
+        case "candidate":
+            peerConn.addIceCandidate(data.candidate)
+    }
+}
