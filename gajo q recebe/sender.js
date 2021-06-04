f5241171310fe19b42686a14258f147bf16d7811
domios5 <85107663+domios5@users.noreply.github.com>
2021-06-04 20:30:09 +0100

    coise

--- gajo q recebe/sender.js
+++ gajo q recebe/sender.js
@@ -1,15 +1,38 @@
 const webSocket = new WebSocket("ws://Localhost:5500")
 
-webSocket.onmessage = (event) => {
-    handleSignallingData(JSON.parse(event.data))
+let username
+function sendUsername(){
+
+    username = document.getElementById("usernem-input").nodeValue
+    sendData({
+        type:"store_user"
+    })
+
 }
 
-function handleSignallingData(data) {
-    switch (data.type) {
-        case "answer":
-            peerConn.setRemoteDescription(data.answer)
-            break
-        case "candidate":
-            peerConn.addIceCandidate(data.candidate)
-    }
+function sendData(data){
+    data.username = username
+    webSocket.send(JSON.stringify(data))
+}
+
+let localStream
+function startCall(){
+    document.getElementById("video-call-div")
+    .style.display = "inline"
+
+    navigator.getUserMedia({
+        video:{
+            frameRate: 24,
+            width: {
+            min:480, indeal:720, max:1280
+            },
+            aspectRatio: 1.333333
+        },
+        audio: true
+    }, (stream) =>{
+        document.getElementById("local-video").srcObject = localStream
+    }, (error)=> {
+        console.log(error)
+    
+    })
 }
